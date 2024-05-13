799e27ddca2ee2d8622eaf2cc66aae2365633c2a
rajkonkret <rajkonkret660@gmail.com>
2024-05-13 13:20:26 +0200

    [1.2] add teksty.py

---
+++ teksty.py
@@ -0,0 +1,40 @@
+tekst = "Witaj świecie"
+print(tekst)  # Witaj świecie
+print(type(tekst))  # <class 'str'>
+
+# teksty są niemutowalne
+tekst.upper()
+""" Return a copy of the string converted to uppercase. """
+print(tekst)  # Witaj świecie
+print(tekst.upper())  # WITAJ ŚWIECIE
+tekst2 = tekst.upper()  # WITAJ ŚWIECIE
+print(tekst2)  # WITAJ ŚWIECIE
+print(tekst)  # Witaj świecie
+
+tekst3 = tekst.lower()
+print(tekst3)  # witaj świecie
+print(tekst)  # Witaj świecie
+
+print(tekst.removesuffix("świecie"))  # "Witaj "
+print(tekst.removeprefix("Witaj"))  # " świecie"
+print(tekst)  # Witaj świecie
+
+tekst4 = "Witaj dobry świecie"
+print(tekst4.replace("dobry ", ""))  # Witaj świecie
+
+print(tekst.count("i"))  # 3
+print(tekst.count("i", 0, 4))  # 1
+# Witaj świecie
+# 0123456789
+# start 0, stop 4
+# Wita 0123 - z prawej strony zbiór otwarty
+print(tekst.count("i", 5, 9))  # " świ" indeksy - 5678, 1
+print(tekst.count("j", 0, 4))  # 0
+print(tekst)  # Witaj świecie
+print(tekst[3])  # "a"
+
+encoded_s = tekst.encode('utf-8')
+print(encoded_s)  # b'Witaj \xc5\x9bwiecie'  b - zapis bajtowy
+# liczba w kodzie szesnastkowym \xc5
+print(type(encoded_s))  # <class 'bytes'>
+print(encoded_s.decode('utf-8'))  # Witaj świecie
