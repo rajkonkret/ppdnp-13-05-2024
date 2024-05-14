a7752f3155ffd473794cbcf41d39fb45a0e287b8
rajkonkret <rajkonkret660@gmail.com>
2024-05-14 11:16:21 +0200

    [2.3] update typy_danych_2_lista.py

--- 1/typy_danych_2_lista.py
+++ 1/typy_danych_2_lista.py
@@ -75,3 +75,78 @@ print(lista)  # ['Krzysztof', 'Marek', 'Janek', 'Radek']
 print(lista.pop())  # Radek - usunie ostatni element z listy
 print(lista)  # ['Krzysztof', 'Marek', 'Janek']
 print(lista.pop(-2))  # Marek
+
+a = 1
+b = 3
+a = b
+print(a)  # 3
+b = 7
+print(a)  # 3
+
+lista_2 = lista  # a = b, kopiowanie adresu (referencji)
+lista_copy = lista.copy()  # kopiowanie elementów jedej listy do drugiej
+print(lista)  # ['Krzysztof', 'Janek']
+print(lista_2)  # ['Krzysztof', 'Janek']
+print(lista_copy)  # ['Krzysztof', 'Janek']
+lista.clear()  # b = 7
+print(lista)  # []
+print(lista_2)  # [], obie wskazuja na ten sam obszar pamieci, zmiana następuje w obu
+print(lista_copy)  # ['Krzysztof', 'Janek']
+print(id(lista))  # 2021573382528
+print(id(lista_2))  # 2021573382528
+print(id(lista_copy))  # 2021576213952
+
+# liczby = [54, 999, 34, 22, 12.34, 687]
+# liczby = [54, 999, 34, 22, 12.34, 687, "A"]  # TypeError: '<' not supported between instances of 'str' and 'int'
+liczby = [54, 999, 34, 22, 12.34, 687, True]  # TypeError: '<' not supported between instances of 'str' and 'int'
+print(type(liczby))  # <class 'list'>
+print(liczby)  # [54, 999, 34, 22, 12.34, 687]
+
+liczby.sort()  # dla list string-int TypeError: '<' not supported between instances of 'str' and 'int'
+print(liczby)  # [12.34, 22, 34, 54, 687, 999] - zmienia bazowa kolekcję
+print(liczby)  # [True, 12.34, 22, 34, 54, 687, 999] dla listy z bool
+
+lista_osob = ['radek', 'ola', 'agata', 'lena']
+lista_osob.sort()
+print(lista_osob)  # ['agata', 'lena', 'ola', 'radek']
+lista_osob.sort(reverse=True)  # sortuje i odwraca kolejność
+print(lista_osob)  # ['radek', 'ola', 'lena', 'agata']
+
+# kody ascii dla znaku "A" i "a"
+print(ord("A"))  # 65
+print(ord("a"))  # 97
+print("radek".capitalize())  # Radek
+
+liczby.reverse()  # odwrócenie listy
+print(liczby)  # [999, 687, 54, 34, 22, 12.34, True]
+
+liczby[3] = 6666
+print(liczby[0:3])  # [999, 687, 54]
+print(liczby[-2])  # 12.34
+print(liczby[-3:])  # [22, 12.34, True]
+print(liczby)  # [999, 687, 54, 6666, 22, 12.34, True]
+
+liczby.remove(54)
+print(liczby)  # [999, 687, 6666, 22, 12.34, True]
+
+print(liczby.pop(2))  # 6666
+print(len(liczby))  # 5
+
+print(liczby)  # [999, 687, 22, 12.34, True]
+print(liczby[0:4:2])  # [999, 22]
+print(liczby[::-1])  # odwrócona lista [True, 12.34, 22, 687, 999]
+
+tekst = "Pyt hon."
+lista1 = list(tekst)  # list() - rzutownie na liste, rozpakowanie sekwencji
+print(lista1)  # ['P', 'y', 't', ' ', 'h', 'o', 'n', '.']
+
+lista3 = [tekst]
+print(lista3)  # ['Python']
+
+liczba = 123456
+# print(list(liczba))  # TypeError: 'int' object is not iterable
+print([liczba])  # [123456]
+
+krotka = tuple(liczby)  # tuple() - rzutowanie na krotkę (tuplę)
+print(type(krotka))  # <class 'tuple'>
+print(krotka)  # (999, 687, 22, 12.34, True)
