62aa7ba83b70c5191ca7e952dd53a3b72fc7e4b6
rajkonkret <rajkonkret660@gmail.com>
2024-05-15 14:38:15 +0200

    [3.10] add son_zad1

---
+++ 3/json_zad1.py
@@ -0,0 +1,28 @@
+import json
+
+# json - {"name":"Radek"}
+# obiekt typu para klucz-wartosc
+# odpowiednikiem json w pythonie jest słownik
+
+person_dict = {'name': "Radek", 'age': 40, 'czy_pali': None}
+print(type(person_dict))  # <class 'dict'>
+
+with open('nasze_dane.json', 'w') as f:
+    json.dump(person_dict, f)
+# {"name": "Radek", "age": 40, "czy_pali": null}
+
+# upiększony i posortowany
+with open('nasze_dane.json', 'w') as f:
+    json.dump(person_dict, f, indent=4, sort_keys=True)
+# {
+#     "age": 40,
+#     "czy_pali": null,
+#     "name": "Radek"
+# }
+
+with open('nasze_dane.json', 'r') as fh:
+    data = json.load(fh)
+
+print(data)  # {'age': 40, 'czy_pali': None, 'name': 'Radek'}
+print(type(data))  # <class 'dict'>
+print(data['name'])  # Radek
