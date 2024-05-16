11bb5b9b87e53d96107da6e5378ff2a0c7e8fdb0
rajkonkret <rajkonkret660@gmail.com>
2024-05-16 14:21:55 +0200

    [4.14] update kl2

--- 4/kl2.py
+++ 4/kl2.py
@@ -20,6 +20,17 @@ class Human:
         print(f"Nazywam się {self.imie}")
 
     # Stworzyc dwie różne metody do wypisanie wieku i wzrostu
+    def wypisz_wzrost(self):
+        print(f"Mam {self.wzrost} cm wzrostu")
+
+    def wypisz_wiek(self):
+        print(f"Mam {self.wiek} lat")
+
+    def ruszaj(self):
+        if self.plec == "k":
+            print("Ruszyłam w drogę")
+        else:
+            print("Ruszyłem w drogę")
 
 
 cz1 = Human("Radek", 56, 178, "m")
@@ -28,3 +39,16 @@ print("Wiek", cz1.wiek)  # Wiek 56
 print("Płeć", cz1.plec)  # Płeć m
 # cz2 = Human() TypeError: Human.__init__() missing 3 required positional arguments: 'imie', 'wiek', and 'wzrost'
 cz1.powitanie()  # Nazywam się Radek
+cz1.wypisz_wzrost()
+cz1.wypisz_wiek()
+# Mam 178 cm wzrostu
+# Mam 56 lat
+cz2 = Human("Anna", 34, 170)
+cz1.ruszaj()
+print("-----")
+cz2.powitanie()
+cz2.ruszaj()
+# Ruszyłem w drogę
+# -----
+# Nazywam się Anna
+# Ruszyłam w drogę
